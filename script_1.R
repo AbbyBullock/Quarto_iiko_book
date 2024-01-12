a2f71f3a3236dcd67b1d27fe51de8d254a170c75
Vladimir Kostyria <abbybullock@HUAWEI_P30_lite-d2b26a85b.localdomain>
2024-01-12 13:46:03 +0300

    Проверка

--- script_1.R
+++ script_1.R
@@ -5,6 +5,7 @@ library(dtplyr)
 library(patchwork)
 library(vroom)
 library(gt)
+
 library(gghighlight)
 
 sales_iiko <- vroom("/Users/abbybullock/Library/Mobile Documents/com~apple~CloudDocs/DDM/Git/iiko/sales_iiko.csv")
